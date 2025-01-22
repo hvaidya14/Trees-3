79ede572aa5da7859dda9aae77b14e86ead9bc73
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-22 14:13:45 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,47 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> sublist = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        preorder(root, sublist, targetSum, 0);
+        return result;
+    }
+    private void preorder(TreeNode root, List<Integer> sublist, int targetSum, int sum) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            sum = sum + root.val;
+            if (targetSum == sum) {
+                sublist.add(root.val);
+                result.add(new ArrayList<>(sublist));
+                sublist.remove(sublist.size()-1);
+            }
+            return;
+        }
+        sublist.add(root.val);
+        sum = sum + root.val;
+        preorder(root.left, sublist, targetSum, sum);
+        preorder(root.right, sublist, targetSum, sum);
+        sublist.remove(sublist.size()-1);
+    }
+}
